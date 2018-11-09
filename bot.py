86c31c876ce236e0c35552b5bcddfd0736a83db2
charm42 <ChadMc.mojang@gmail.com>
2018-11-09 01:25:27 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -119,4 +119,4 @@ async def create_vote(ctx,
 	await bot.say("** **")
 	await bot.delete_message(ctx.message)
 
-bot.run(os.getenv('TOKEN'))
+bot.run("NTA2Mjc2OTEwOTA5NzUxMzQ3.DsZjgA.mWQGqBuqgoBjznztqibnALr6FiM")
